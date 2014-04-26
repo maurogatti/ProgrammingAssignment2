c5013e658c308481caf0ae8b3acf9e7ab22ed31e
maurogatti <mauro_gatti@yahoo.it>
2014-04-26 18:40:12 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This R script file contains four functions
+#The function makeCacheMatrix creates the special vector required by cacheSolve function
+#The function cacheSolve computes the inverse matrix or retrieves it from cache (when available)
+#The function testCacheMatrix1 is used to verify that the method cacheSolve actually returns the inverse matrix
+#The function testCacheMatrix2 is used to verify that the method cacheSolve actually retrieves the data item from cache when possible
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Function that creates the special vector used by cacheSolve function
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setCacheMatrix <- function(solve) m <<- cacheSolve
+        getCacheMatrix <- function() m
+        list(set = set, get = get,
+             setCacheMatrix = setCacheMatrix,
+             getCacheMatrix = getCacheMatrix)
 }
 
-
-## Write a short comment describing this function
-
+#Function that computes the inverse matrix or retrieves it from cache whenever value has been cached
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getCacheMatrix()
+        if(!is.null(m)) {
+                message("Successful test. Getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setCacheMatrix(m)
+        m
 }
+
+#Unit test 1: the function should return Successul test. This test verifies that
+#the returned matrix is actually the inverse matrix
+testCacheMatrix1 <- function() {
+        n<-sample(20,1); #matrix to be inverted dimension
+        testMatrix<-matrix(rnorm(n^2,0,1),n,n); #matrix to be inverted
+        inverseMatrix<-cacheSolve(makeCacheMatrix(testMatrix));
+        if(isTRUE(all.equal(testMatrix%*%inverseMatrix,diag(n))))
+        {
+                message("Successful test. The cached matrix is the inverse matrix.")     
+        }
+        else
+        {
+                message("Unsuccessful test. The cached matrix is not the inverse matrix.") 
+        }   
+}
+
+#Unit test 2: the function should return Successul test. This test verifies that 
+#the value is actually returned from cache
+testCacheMatrix2 <- function() {
+        n<-sample(20,1); #matrix to be inverted dimension
+        testMatrix<-matrix(rnorm(n^2,0,1),n,n); #matrix to be inverted
+        testCacheMatrix<-makeCacheMatrix(testMatrix)
+        inverseMatrix<-cacheSolve(testCacheMatrix);
+        inverseMatrix<-cacheSolve(testCacheMatrix);
+}
+
